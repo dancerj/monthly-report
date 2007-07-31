4c2b6ae856acba99ac2926ad8096072d07333197
Junichi Uekawa <dancer@dancer64.netfort.gr.jp>
2007-08-01 00:06:17 +0900

    fix text on graph

--- image200712/people-chart.py
+++ image200712/people-chart.py
@@ -5,17 +5,18 @@
 from pychart import *
 
 theme.use_color=True
-theme.reinitialize()
 theme.default_font_size=26
+theme.default_line_width=2
+theme.reinitialize()
 
 data = chart_data.read_csv('people.csv')
-xaxis = axis.X(tic_interval = 1, label="Time")
+xaxis = axis.X(tic_interval = 1, label="Month")
 yaxis = axis.Y(tic_interval = 10, label="Attendees")
 chart_object.set_defaults(area.T, size = (480, 300))
 ar = area.T(x_axis=xaxis, y_axis=yaxis, x_range=(1,12), y_range=(0,None))
-plot2005 = line_plot.T(label="year 2005", data=data, ycol=1, tick_mark=tick_mark.circle2)
-plot2006 = line_plot.T(label="year 2006", data=data, ycol=2, tick_mark=tick_mark.square)
-plot2007 = line_plot.T(label="year 2007", data=data, ycol=3, tick_mark=tick_mark.square)
+plot2005 = line_plot.T(label="year 2005", data=data, ycol=1)
+plot2006 = line_plot.T(label="year 2006", data=data, ycol=2)
+plot2007 = line_plot.T(label="year 2007", data=data, ycol=3)
 ar.add_plot(plot2005, plot2006, plot2007)
 can = canvas.init("people-chart.png")
 ar.draw(can)
