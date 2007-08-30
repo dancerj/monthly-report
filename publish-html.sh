741f1ccb3bdb3af25f933c8a51ab4eb3d6112012
Junichi Uekawa <dancer@dancer64.netfort.gr.jp>
2007-08-31 00:10:04 +0900

    revise for tex4ht, and toolchain update for tex4ht

--- publish-html.sh
+++ publish-html.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 sudo apt-get install -y tex4ht dvi2ps-fontdata-a2n dvi2dvi dvipng
 for A in *.tex; do
-    ./htplatex $A jp,2,sections+
+    case $A in 
+	*presentation*) ;;
+	*natsu*) ;;
+	*fuyu*) ;;
+	*) 
+	    ./htplatex $A jp,2,sections+
+	    ;;
+    esac
 done
 sudo apt-get remove -y dvi2ps-fontdata-a2n
+rm *.dvi
+
+# c.f. bug #XXXX
+for A in html/*.html; do
+    sed -i -e 's,src="image.*/,src=",' "$A"
+    sed -i -f tex4ht/dnp-trim.sed "$A"
+    rm "$A.tmp"
+    while ! diff "$A.tmp" "$A" > /dev/null ; do
+	mv "$A.tmp" "$A" || true
+	echo "Modifying $A..."
+	sed -f tex4ht/dnp-span.sed "$A" > "$A.tmp"
+    done
+done
