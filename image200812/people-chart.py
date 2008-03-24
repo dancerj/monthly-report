58926919eb636d978f28704d8e038e41f4cc6f51
Junichi Uekawa <dancer@netfort.gr.jp>
2008-03-24 23:08:16 +0900

    add 200812 text template, and fill in the initial values.

---
+++ image200812/people-chart.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# http://home.gna.org/pychart/doc/index.html
+# apt-get install python-pychart
+
+from pychart import *
+
+theme.use_color=True
+theme.default_font_size=26
+theme.default_line_width=2
+theme.reinitialize()
+
+data = chart_data.read_csv('people.csv')
+xaxis = axis.X(tic_interval = 1, label="Month")
+yaxis = axis.Y(tic_interval = 10, label="Attendees")
+chart_object.set_defaults(area.T, size = (480, 300))
+ar = area.T(x_axis=xaxis, y_axis=yaxis, x_range=(1,12), y_range=(0,None))
+plot2005 = line_plot.T(label="year 2005", data=data, ycol=1)
+plot2006 = line_plot.T(label="year 2006", data=data, ycol=2)
+plot2007 = line_plot.T(label="year 2007", data=data, ycol=3)
+plot2008 = line_plot.T(label="year 2008", data=data, ycol=3)
+ar.add_plot(plot2005, plot2006, plot2007, plot2008)
+can = canvas.init("people-chart.png")
+ar.draw(can)
