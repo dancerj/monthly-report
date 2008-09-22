1ed95b21839c2644febf7f08bf46feec40298efa
Junichi Uekawa <dancer@netfort.gr.jp>
2008-09-21 21:53:08 -0700

    jabber bot: properly handle group chats.

--- jabberbot/xmpptest.py
+++ jabberbot/xmpptest.py
@@ -5,57 +5,73 @@ import sys
 import os 
 import xmpp
 
-# conference group name and password
-CONF=('hackathon@conference.coreduo.local','')
+class JabberConferenceLogger:
+    # conference group name and password
+    CONF=('hackathon@conference.coreduo.local','')
+    # proxy configuration.
+    PROXY={}
 
-# proxy configuration.
-PROXY={}
+    def messagehandler(self, sess, mess):
+        nick=mess.getFrom().getResource()
+        text=mess.getBody()
+        type=mess.getType()
+        group=mess.getFrom().getStripped()
+        if (type=="chat" or type=="groupchat") and text:
+            reply = mess.buildReply(text)
+            # if message came from groupchat, respond to the group.
+            if type == "groupchat" :
+                reply.setTo(group)
+                reply.setType("groupchat")
+            if nick == self.jid.getNode(): # except my own message, to avoid infinite loop
+                return 
+            print (u"%s: %s: %s: %s\n"
+                   %(type, 
+                     group, 
+                     nick, text)).encode('utf-8')
+            sess.send(reply)
 
-def messagehandler(sess, mess):
-    nick=mess.getFrom().getResource()
-    text=mess.getBody()
-    if text:
-        print (u"%s: %s\n"%(mess.getType(), text)).encode('utf-8')
-    reply = mess.buildReply(text)
-    sess.send(reply)
+    def presencehandler(self, sess, mess):
+        """Called when a presence is recieved"""
+        who = str(mess.getFrom())
+        type = mess.getType()
+        if type == None: type = 'available'
 
-def presencehandler(sess, mess):
-    """Called when a presence is recieved"""
-    who = str(mess.getFrom())
-    type = mess.getType()
-    if type == None: type = 'available'
+        if type == 'subscribe':
+            sess.send(xmpp.protocol.Presence(to=who, typ='subscribed'))
+            sess.send(xmpp.protocol.Presence(to=who, typ='subscribe'))
 
-    if type == 'subscribe':
-        sess.send(xmpp.protocol.Presence(to=who, typ='subscribed'))
-        sess.send(xmpp.protocol.Presence(to=who, typ='subscribe'))
-        
-    elif type == 'unsubscribe':
-        sess.send(xmpp.protocol.Presence(to=who, typ='unsubscribed'))
-        sess.send(xmpp.protocol.Presence(to=who, typ='unsubscribe'))
-    
-def ConnectJabber(id, password):
-    jid = xmpp.protocol.JID(id)
-    sys.stdout.write(jid.getDomain())
-    cl = xmpp.Client(jid.getDomain(),debug=[])
-    cl.connect(proxy=PROXY)
-    cl.auth(jid.getNode(), password)
-    cl.RegisterHandler('message', messagehandler)
-    cl.RegisterHandler('presence', presencehandler)
-    cl.sendInitPresence()
-    
-    # join conference room
-    p=xmpp.protocol.Presence(to='%s/%s'%(CONF[0],jid.getNode()))
-    p.setTag('x',namespace=xmpp.NS_MUC).setTagData('password',CONF[1])
-    p.getTag('x').addChild('history',{'maxchars':'0','maxstanzas':'0'})
-    cl.send(p)
+        elif type == 'unsubscribe':
+            sess.send(xmpp.protocol.Presence(to=who, typ='unsubscribed'))
+            sess.send(xmpp.protocol.Presence(to=who, typ='unsubscribe'))
 
-    return cl
+    def ConnectJabber(self, id, password):
+        self.jid = xmpp.protocol.JID(id)
+        sys.stdout.write(self.jid.getDomain())
+        cl = xmpp.Client(self.jid.getDomain(),debug=[])
+        cl.connect(proxy=self.PROXY)
+        cl.auth(self.jid.getNode(), password)
+        cl.RegisterHandler('message', self.messagehandler)
+        cl.RegisterHandler('presence', self.presencehandler)
+        cl.sendInitPresence()
+
+        # join conference room
+        p=xmpp.protocol.Presence(to='%s/%s'%(self.CONF[0],self.jid.getNode()))
+        p.setTag('x',namespace=xmpp.NS_MUC).setTagData('password',self.CONF[1])
+        p.getTag('x').addChild('history',{'maxchars':'0','maxstanzas':'0'})
+        cl.send(p)
+        self.jabberclient = cl
+        return cl
+
+    def __init__ (self):
+        # Call Connect Jabber.
+        return
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         sys.stderr.write("%s: username@jabbernetwork password\n"%sys.argv[0])
         exit()
-    cl = ConnectJabber(sys.argv[1], sys.argv[2])
+    a = JabberConferenceLogger()
+    cl = a.ConnectJabber(sys.argv[1], sys.argv[2])
     while 1: 
         cl.Process(1)
 
