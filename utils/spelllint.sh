05d2d600c94d9317286f118142926e37e1c5ac8b
Junichi Uekawa <dancer@netfort.gr.jp>
2009-01-12 11:02:17 +0900

    add verbose message for spell lint tool.

--- utils/spelllint.sh
+++ utils/spelllint.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+if ! which lgrep; then
+    echo 'ERROR: Please install lgrep from lv package'
+fi
+
 if lgrep -i -n \
     'debain\|．\|，\|東京Debian勉強会' \
     /dev/null "$@"  ; then
+    echo 'ERROR: lgrep for wrong keyword failed. Please check.'
     exit 1
 else
     exit 0
