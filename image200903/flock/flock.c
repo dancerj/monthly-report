20697266271f17b98283fc19131495973d35fc52
Junichi Uekawa <dancer@netfort.gr.jp>
2009-03-05 12:36:05 +0900

    add annotations about flock.

--- image200903/flock/flock.c
+++ image200903/flock/flock.c
@@ -18,12 +18,15 @@ int main(int argc, char** argv)
       if (-1==fd) 
 	{
 	  perror("open");
+	  /* this will usually not fail unless there is something
+	     wrong with the file */
 	  return 1;
 	}
       
       if (-1==flock(fd, LOCK_EX)) 
 	{
 	  perror("flock");
+	  /* This will usually not fail. It will wait. */
 	  return 1;
 	}
       
@@ -33,9 +36,12 @@ int main(int argc, char** argv)
       printf("done\n");
       if (-1==flock(fd, LOCK_UN)) 
 	{
+	  /* This does not usually fail */
 	  perror("flock");
 	  return 1;
 	}
+      printf("Unlocked file.\n");
+
       close(fd);
     }
   else
