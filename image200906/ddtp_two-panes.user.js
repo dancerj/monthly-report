a52cc380ce0e1928e0a858a0cfa422012ce7427b
Junichi Uekawa <dancer@netfort.gr.jp>
2009-06-19 23:46:33 +0900

    get Greasemonkey script to work with both new translation and review.

--- image200906/ddtp_two-panes.user.js
+++ image200906/ddtp_two-panes.user.js
@@ -3,13 +3,18 @@
 // @namespace      ddtptwo
 // @description    Display DDTP in two-panes mode
 // @include        http://ddtp.debian.net/ddtss/index.cgi/*/translate/*
+// @include        http://ddtp.debian.net/ddtss/index.cgi/*/forreview/*
 // ==/UserScript==
 
 (function() {
-    // get the third UL, and name it floatie
+    // Get the third UL, and name it floatie.
     document.getElementsByTagName("ul")[2].setAttribute("id", "floatie");
+    // Do not show the diff since it's rather useless.
     var style =
 	<><![CDATA[
+		   div.diff {
+		       display: none;
+		   }
 		   ul#floatie > li {
 		       float: left;
 		       width: 50%;
