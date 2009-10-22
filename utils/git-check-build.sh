0dfe707a7cf6092ef6f3e4b112976acad2205e13
Junichi Uekawa <dancer@netfort.gr.jp>
2009-10-23 00:09:15 +0900

    Use git checkout-index instead of git-build for check-build script.

--- utils/git-check-build.sh
+++ utils/git-check-build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # check build in a different directory
+set -e
+
 TEMPDIR=$(mktemp -d)
 
-git archive --format=tar HEAD | \
-    ( cd ${TEMPDIR} &&  
-    tar xvf - &&
-    mkdir -p .git/hooks &&
-    cp git-pre-commit.sh .git/hooks/pre-commit &&
-    make -j5 )
+git checkout-index --prefix=${TEMPDIR}/ -a
+cd ${TEMPDIR}
+mkdir -p .git/hooks
+cp git-pre-commit.sh .git/hooks/pre-commit
+make -j5
 
