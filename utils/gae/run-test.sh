bf4cbf8075e45b46735ce0634514ecbdf1f3c298
Junichi Uekawa <dancer@netfort.gr.jp>
2011-12-17 16:42:11 +0900

    fix test setup.
    It didn't work on the latest SDK version.

--- utils/gae/run-test.sh
+++ utils/gae/run-test.sh
@@ -3,5 +3,7 @@
 
 GAE_PATH=$(dirname $(readlink -f $(which dev_appserver.py)))
 
-PYTHONPATH=$GAE_PATH:$GAE_PATH/lib/django/:. python ./testSystem.py
+# Try to force 0.96 for test on more recent appengine SDKs
+# e.g. 1.6.1
+PYTHONPATH=$GAE_PATH:$GAE_PATH/lib/django_0_96:. python ./testSystem.py
 
