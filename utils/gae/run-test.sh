08244ce4009c2cece34b3e4f247078245216b135
Junichi Uekawa <dancer@netfort.gr.jp>
2011-12-25 20:26:07 +0900

    remove run-test.sh and do the test environment setup in Makefile.

--- utils/gae/run-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Requires dev_appserver.py to be in the path.
-
-GAE_PATH=$(dirname $(readlink -f $(which dev_appserver.py)))
-
-# Try to force 0.96 for test on more recent appengine SDKs
-# e.g. 1.6.1
-PYTHONPATH=$GAE_PATH:$GAE_PATH/lib/django_0_96:. python ./testSystem.py
-
