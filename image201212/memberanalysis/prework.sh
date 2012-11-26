7aa8bf5fb40fae74252c92d649f0e6aa28ef1199
Junichi Uekawa <dancer@netfort.gr.jp>
2012-11-27 07:10:32 +0900

    handcraft information from available sources in the monthly reports.
    They are somewhat sparse.

--- image201212/memberanalysis/prework.sh
+++ image201212/memberanalysis/prework.sh
@@ -10,8 +10,6 @@ for A in debianmeetingresume200[78]??.tex; do echo $A; iconv -f iso-2022-jp -t e
 # 2009 prework format
 for A in image2009??/prework.tex; do echo $A; iconv -f iso-2022-jp -t euc-jp $A | awk ' { if ($0 ~ /\\begin{prework}{(.*)}/ ) {gsub(/\\begin{prework}{/,""); gsub(/}/,""); print $0}}' ; done 
 
-# 2010 prework format; unfortunately, some are in EUC-JP, some are in ISO-2022-JP.
-for A in image2010??/prework.tex; do echo $A;  nkf -w $A | awk ' { if ($0 ~ /\\begin{prework}{(.*)}/ ) {gsub(/\\begin{prework}{/,""); gsub(/}/,""); print $0}}' ; done 
+# 2010-2012 prework format; unfortunately, some are in EUC-JP, some are in ISO-2022-JP.
+for A in image201???/prework.tex; do echo $A;  nkf -w $A | awk ' { if ($0 ~ /\\begin{prework}{(.*)}/ ) {gsub(/\\begin{prework}{/,""); gsub(/}/,""); value =value "" $0 ","}} END {print value}' ; done 
 
-# 2011 prework format; unfortunately, some are in EUC-JP, some are in ISO-2022-JP.
-for A in image2011??/prework.tex; do echo $A;  nkf -w $A | awk ' { if ($0 ~ /\\begin{prework}{(.*)}/ ) {gsub(/\\begin{prework}{/,""); gsub(/}/,""); print $0}}' ; done 
