29093332946647e6974f08316ca19981ac65fc59
Junichi Uekawa <dancer@netfort.gr.jp>
2012-11-29 07:32:21 +0900

    fix makefile and fix scripts to tweak the visual output.

--- image201212/memberanalysis/tokyograph.R
+++ image201212/memberanalysis/tokyograph.R
@@ -2,11 +2,21 @@
 # Script to generate graphs for Tokyo, using csv exported by Sqlite job.
 
 attend <- read.csv("attend.csv")
+attend$attend
 postscript('attend.eps')
-plot(filter(attend$attend, rep(1/12,12), sides=1), type='s', xlab='nth Tokyo Area Debian Meeting', ylab='number of attendees (12m MA)')
+plot(filter(attend$attend, rep(1/12,12), sides=1),
+  type='s',
+  xlab='nth Tokyo Area Debian Meeting',
+  ylab='number of attendees (12m MA)',
+  ylim=range(0,20))
 dev.off()
+attend$prework
 postscript('prework.eps')
-plot(filter(attend$prew, rep(1/12,12), sides=1), type='s', xlab='nth Tokyo Area Debian Meeting', ylab='number of prework (12m MA)')
+plot(filter(attend$prework, rep(1/12,12), sides=1),
+  type='s',
+  xlab='nth Tokyo Area Debian Meeting',
+  ylab='number of prework (12m MA)',
+  ylim=range(0,20))
 dev.off()
 
 
