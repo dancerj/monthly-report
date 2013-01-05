c7d0a6079873690191c862118fa5ed1461fd7275
Junichi Uekawa <dancer@netfort.gr.jp>
2013-01-05 17:17:22 +0900

    review R scripts and add some images.

--- image201301/getenquete.R
+++ image201301/getenquete.R
@@ -28,30 +28,3 @@ enquete_frame <- data.frame(
 
 write.csv(enquete_frame)
 
-# plot a few diagrams.
-postscript('scaled_average_score_density.eps')
-plot(density(enquete_frame$scaled_average_score, na.rm=TRUE), 
-  xlab='scaled average score',
-  ylab='density')
-dev.off()
-
-postscript('raw_average_score_density.eps')
-plot(density(enquete_frame$raw_average_score, na.rm=TRUE), 
-  xlab='raw average score',
-  ylab='density')
-dev.off()
-
-postscript('raw_standard_deviation_density.eps')
-plot(density(enquete_frame$raw_standard_deviation, na.rm=TRUE),
-  xlab='standard deviation of score per event',
-  ylab='density')
-dev.off()
-
-postscript('enquete_boxplot.eps')
-boxplot(enquete)
-dev.off()
-
-postscript('raw_average_score_growth.eps')
-plot(enquete_frame$raw_average_score)
-dev.off()
- 
