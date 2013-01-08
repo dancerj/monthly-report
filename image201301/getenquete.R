0f5c76343a4ae40b6cb7fee7d6e4476a004f13f6
Junichi Uekawa <dancer@netfort.gr.jp>
2013-01-09 07:54:55 +0900

    more study on enquete. Restricting to more-high-confidence people for enquete because they seem to give better feedback.

--- image201301/getenquete.R
+++ image201301/getenquete.R
@@ -5,28 +5,31 @@
 # use "source('getenquete.R')" to use this script interactively.
 enquete <- read.csv('enquete.csv')
 
-# get number of non-na reponse from enquete. Useful for understanding the statistical significance along with SD.
-enquete_response <- colSums(!is.na(enquete))
+compute_enquete_stats <- function(enquete) {
+  # get number of non-na reponse from enquete. Useful for understanding the statistical significance along with SD.
+  enquete_response <- colSums(!is.na(enquete))
 
-# get average numbers for event.
-raw_average_score <- apply(enquete, 2, mean, na.rm=TRUE)
+  # get average numbers for event.
+  raw_average_score <- apply(enquete, 2, mean, na.rm=TRUE)
 
-# get standard deviation for event score.
-raw_standard_deviation <- apply(enquete, 2, sd, na.rm=TRUE)
+  # get standard deviation for event score.
+  raw_standard_deviation <- apply(enquete, 2, sd, na.rm=TRUE)
 
-# scale scores to normal distribution for each user, so that each user's pattern only contributes as much.
-scaled <- t(scale(t(enquete), apply(t(enquete), 2, mean, na.rm=TRUE), apply(t(enquete), 2, sd, na.rm=TRUE)))
-scaled_average_score <- apply(scaled, 2, mean, na.rm=TRUE)
+  # scale scores to normal distribution for each user, so that each user's pattern only contributes as much.
+  scaled <- t(scale(t(enquete), apply(t(enquete), 2, mean, na.rm=TRUE), apply(t(enquete), 2, sd, na.rm=TRUE)))
+  scaled_average_score <- apply(scaled, 2, mean, na.rm=TRUE)
 
-# create a dataframe to join them for easy consumption.
+  # create a dataframe to join them for easy consumption.
 
-enquete_frame <- data.frame(
+  enquete_frame <- data.frame(
 	      raw_average_score = raw_average_score, 
 	      raw_standard_deviation = raw_standard_deviation, 
 	      scaled_average_score = scaled_average_score, 
 	      enquete_response = enquete_response)
 
-write.csv(enquete_frame)
+  enquete_frame
+}
+
 
 dump_for_session <- function(name) {
   # Dump information for a session.
@@ -39,3 +42,10 @@ dump_for_session <- function(name) {
     raw_standard_deviation[name],
     enquete_response[name]), c(3,2)))
 }
+
+enquete_frame <- compute_enquete_stats(enquete)
+write.csv(enquete_frame)
+
+high_confidence_enquete <- subset(enquete, rowSums(!is.na(enquete)) > 5)
+high_confidence_enquete_frame <- compute_enquete_stats(high_confidence_enquete)
+write.csv(high_confidence_enquete_frame)
