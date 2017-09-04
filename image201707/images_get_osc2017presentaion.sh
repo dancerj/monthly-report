6f468dc863de6f24695c75964802c5ff9df97441
Norimitsu Sugimoto <dictoss@live.jp>
2017-09-04 23:08:46 +0900

    add 2017-osc presentations' image and images download script. - 201707-osc-hokkaido - 201709-osc-tokyo-fall

---
+++ image201707/images_get_osc2017presentaion.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# presentation images get program.
+#
+wget https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/DebianFamilyTree1210.svg/500px-DebianFamilyTree1210.svg.png -O 500px-DebianFamilyTree1210.png
+wget "http://ftpmirror.your.org/pub/wikimedia/images/wikipedia/ar/c/c1/523px-Freebsd_logo.svg.png" -O 523px-Freebsd_logo.png
+wget "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/NewTux.svg/625px-NewTux.svg.png" -O 625px-NewTux.png
+wget "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Hurd-logo.svg/600px-Hurd-logo.svg.png" -O Hurd-logo.png
+wget "https://wiki.debian.org/DebianArt/Themes/softWaves?action=AttachFile&do=get&target=attachment_wallpaper.png" -O attachment_wallpaper.png
+wget "https://media.debconf.org/dc15/photos/group_photo_t.jpg"
+wget "https://vignette2.wikia.nocookie.net/villains/images/8/8f/Stretch_TS3.png/revision/latest?cb=20170213021710" -O stretch.png
+
+#
+# stretch-arch-requalification.png get screenshot.
+#   https://release.debian.org/stretch/arch_qualify.html
+#
