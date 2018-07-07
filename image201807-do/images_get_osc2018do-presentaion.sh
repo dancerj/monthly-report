482a0752c895e6b434f97efb7c1bf03466d6e91a
Youhei SASAKI <uwabami@gfd-dennou.org>
2018-07-07 16:47:11 +0900

    Post update: OSC 2018 Hokkaido
    Signed-off-by: Youhei SASAKI <uwabami@gfd-dennou.org>

---
+++ image201807-do/images_get_osc2018do-presentaion.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# presentation images get program.
+#
+wget https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/DebianFamilyTree1210.svg/500px-DebianFamilyTree1210.svg.png -O 500px-DebianFamilyTree1210.png
+wget "http://ftpmirror.your.org/pub/wikimedia/images/wikipedia/ar/c/c1/523px-Freebsd_logo.svg.png" -O 523px-Freebsd_logo.png
+wget "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/NewTux.svg/625px-NewTux.svg.png" -O 625px-NewTux.png
+wget "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Hurd-logo.svg/600px-Hurd-logo.svg.png" -O Hurd-logo.png
+wget "https://wiki.debian.org/DebianArt/Themes/softWaves?action=AttachFile&do=get&target=attachment_wallpaper.png" -O attachment_wallpaper.png
+wget "https://vignette2.wikia.nocookie.net/villains/images/8/8f/Stretch_TS3.png/revision/latest?cb=20170213021710" -O stretch.png
+wget "https://media.debconf.org/dc15/photos/group_photo_t.jpg" -O debconf_2015.jpg
+wget "https://jonathancarter.org/files/images/JJS_0280.jpg" -O debconf_2016.jpg
+wget "https://annex.debconf.org/debconf-share/debconf17/photos/aigarius/debcond17%20group%20photo%20mobile.jpg" -O debconf_2017.jpg
+wget "http://image.gihyo.co.jp/assets/images/cover/2017/thumb/TH160_641711.jpg" -O SD201711.jpg
+wget "https://books.google.co.jp/books/content?id=WVj-CwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73MdDMeHyswd_DRP6lH0lXb2aXHsVdmt1CjY7jsW7sGyqjKCoCmUhlAvl8Kn5_w42rt19lfLr6TLFiNKTTPD0Vbbn8VRwIyeMDKW3P8_eGvJMunLyALZimVgqvVtBEjdQ3BLSzJ" -O DebianHandbook.jpg
+#
+# stretch-arch-requalification.png get screenshot.
+#   https://release.debian.org/stretch/arch_qualify.html
